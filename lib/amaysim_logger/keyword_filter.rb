7558a3598bff3a150dd15e13f4d418e0be670332
lian.ma <lian.ma@Lian-Mas-MacBook-Pro.local>
2017-08-07 16:11:14 +1000

    fix the bug for filtering json message

--- lib/amaysim_logger/keyword_filter.rb
+++ lib/amaysim_logger/keyword_filter.rb
@@ -31,7 +31,7 @@ class AmaysimLogger
       def filter_json(content, filtered_keywords)
         return content unless content.is_a?(String)
         json = JSON.parse(content)
-        filter_hash(json, filtered_keywords).to_json
+        filter_hash(json, filtered_keywords).to_json if content.respond_to?(:keys)
       rescue
         content
       end
